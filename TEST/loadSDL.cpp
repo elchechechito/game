6dbb84d2ca1b6cd160a6d504c63a21a777bee14f
Jose Ramirez <jose.ramirez@gameloft.com>
2017-02-14 12:49:47 -0800

    Add files

---
+++ TEST/loadSDL.cpp
@@ -0,0 +1,175 @@
+﻿#include "loadSDL.h"
+
+loadSDL::loadSDL()
+{
+}
+
+loadSDL::~loadSDL()
+{
+}
+
+bool loadSDL::init()
+{
+	//Initialization flag
+	bool success = true;
+
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	else
+	{
+		//Set texture filtering to linear
+		if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
+		{
+			printf("Warning: Linear texture filtering not enabled!");
+		}
+
+		//Create window
+		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else
+		{
+			//Create vsynced renderer for window
+			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+			if (gRenderer == NULL)
+			{
+				printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+				success = false;
+			}
+			else
+			{
+				//Initialize renderer color
+				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+				//Initialize PNG loading
+				int imgFlags = IMG_INIT_PNG;
+				if (!(IMG_Init(imgFlags) & imgFlags))
+				{
+					printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+					success = false;
+				}
+			}
+		}
+	}
+
+	return success;
+	
+}
+
+void loadSDL::close()
+{
+	//Free loaded images
+	//gTexturePuyo.free();
+
+	//Destroy window	
+	SDL_DestroyRenderer(gRenderer);
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+	gRenderer = NULL;
+
+	//Quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
+}
+
+bool loadSDL::loadMedia()
+{
+	//Loading success flag
+	bool success = true;
+
+	gSpriteSheetTexture = loadTexture("14_animated_sprites_and_vsync/foo.png");
+
+	//Load sprite sheet texture
+	if (gSpriteSheetTexture.texture == NULL)
+	{
+		printf("Failed to load walking animation texture!\n");
+		success = false;
+	}
+	else
+	{
+		//Set sprite clips
+		gSpriteClips[0].x = 0;
+		gSpriteClips[0].y = 0;
+		gSpriteClips[0].w = 64;
+		gSpriteClips[0].h = 205;
+
+		gSpriteClips[1].x = 64;
+		gSpriteClips[1].y = 0;
+		gSpriteClips[1].w = 64;
+		gSpriteClips[1].h = 205;
+
+		gSpriteClips[2].x = 128;
+		gSpriteClips[2].y = 0;
+		gSpriteClips[2].w = 64;
+		gSpriteClips[2].h = 205;
+
+		gSpriteClips[3].x = 196;
+		gSpriteClips[3].y = 0;
+		gSpriteClips[3].w = 64;
+		gSpriteClips[3].h = 205;
+	}
+
+	return success;
+}
+
+loadSDL::Texture loadSDL::loadTexture(std::string path)
+{
+	Texture loadTexture;
+
+	//The final texture
+	SDL_Texture* newTexture = NULL;
+
+	//Load image at specified path
+	SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+	if (loadedSurface == NULL)
+	{
+		printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
+	}
+	else
+	{
+		//
+		SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 255, 0, 0));
+
+		//Create texture from surface pixels
+		newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+		if (newTexture == NULL)
+		{
+			printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+		}
+		else
+		{
+			loadTexture.textureWidth = loadedSurface->w;
+			loadTexture.textureHeight = loadedSurface->h;
+		}
+
+		//Get rid of old loaded surface
+		SDL_FreeSurface(loadedSurface);
+	}
+
+	loadTexture.texture = newTexture;
+
+	return loadTexture;
+}
+
+void loadSDL::render(int x, int y, loadSDL::Texture renderTexture, SDL_Rect * clip)
+{
+	//Set rendering space and render to screen
+	SDL_Rect renderQuad = { x, y, renderTexture.textureWidth, renderTexture.textureHeight };
+	
+	//Set clip rendering dimensions
+	if (clip != NULL)
+	{
+		renderQuad.w = clip->w;
+		renderQuad.h = clip->h;
+	}
+	
+	//Render to screen
+	SDL_RenderCopy(gRenderer, renderTexture.texture, clip, &renderQuad);
+}
---
+++ TEST/loadSDL.h
@@ -0,0 +1,57 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+#include <string>
+
+#pragma once
+class loadSDL
+{
+public:
+	
+	struct Texture {
+		SDL_Texture* texture;
+		int textureWidth;
+		int textureHeight;
+	};
+	
+	loadSDL();
+	~loadSDL();
+
+	//Starts up SDL and creates window
+	bool init();
+
+	//Frees media and shuts down SDL
+	void close();
+
+	//Loads media
+	bool loadMedia();
+
+	//Load Texture
+	Texture loadTexture(std::string path);
+
+	//Renders texture at given point
+	void render(int x, int y, loadSDL::Texture renderTexture,SDL_Rect* clip = NULL);
+
+	//The window we'll be rendering to
+	SDL_Window* gWindow = NULL;
+
+	//The window renderer
+	SDL_Renderer* gRenderer = NULL;
+
+	//Texture to load puyo
+	Texture gTexturePuyo;
+
+	//Texture to load background
+	Texture gTextureBackground;
+
+	//Texture
+	Texture gSpriteSheetTexture;
+	
+	//screen size
+	int SCREEN_WIDTH = 450;
+	int SCREEN_HEIGHT = 600;
+
+	SDL_Rect gSpriteClips[4];
+
+};
+
